30debdb602b223122bd0b56db7e3bb01f0ff9988
InnovativeName-GameDev <david.simon11melone@gmail.com>
2026-03-01 10:15:59 +0100

    iso test 10

---
+++ modules/vm-disko-config.nix
@@ -0,0 +1,19 @@
+{
+  disko.devices.disk.main = {
+    device = "/dev/sda";
+    type = "disk";
+    content = {
+      type = "gpt";
+      partitions = {
+        root = {
+          size = "100%";
+          content = {
+            type = "filesystem";
+            format = "ext4";
+            mountpoint = "/";
+          };
+        };
+      };
+    };
+  };
+}
