97936a5639feb051cbf7c1526b3dcbf37faa6fa4
InnovativeName-GameDev <david.simon11melone@gmail.com>
2026-03-01 10:50:27 +0100

    iso test 13

--- hosts/nginx-homeserver/configuration.nix
+++ hosts/nginx-homeserver/configuration.nix
@@ -12,9 +12,7 @@
 
   # Bootloader.
   boot.loader.grub.enable = true;
-  boot.loader.grub.efiSupport = true;
-  boot.loader.grub.device = "nodev";
-  boot.loader.efi.canTouchEfiVariables = true;
+  boot.loader.grub.device = "/dev/sda";
 
   networking.hostName = "pve-nixos-test3"; # Define your hostname.
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
--- modules/vm-disko-config.nix
+++ modules/vm-disko-config.nix
@@ -1,5 +1,3 @@
-{ ... }:
-
 {
   disko.devices.disk.main = {
     device = "/dev/sda";
@@ -7,14 +5,9 @@
     content = {
       type = "gpt";
       partitions = {
-        ESP = {
-          size = "512M";
-          type = "EF00";
-          content = {
-            type = "filesystem";
-            format = "vfat";
-            mountpoint = "/boot";
-          };
+        bios = {
+          size = "1M";
+          type = "EF02";
         };
 
         root = {
