d6ba352f506e9e66c0264b82e1a287c03ae2d778
InnovativeName-GameDev <david.simon11melone@gmail.com>
2026-03-07 07:18:47 +0100

    iso test

--- iso/install.sh
+++ iso/install.sh
@@ -2,22 +2,22 @@
 set -euxo pipefail
 
 FLAKE_REPO="https://github.com/InnovativeName-GameDev/homeserver.git"
+FLAKE_DIR="/mnt/flake"
 HOST="test-vm"
 
 echo "Waiting for network..."
 until ping -c1 github.com >/dev/null 2>&1; do sleep 1; done
 
-echo "Running disko..."
-# This will format and mount partitions
-disko --mode destroy,format,mount "$FLAKE_DIR/modules/vm-disko-config.nix"
-
 # At this point, root is mounted at /mnt
 # Make a directory for the flake inside the new root
-FLAKE_DIR="/mnt/flake"
 mkdir -p "$FLAKE_DIR"
 
 # Clone the flake now that /mnt exists
 git clone "$FLAKE_REPO" "$FLAKE_DIR"
+
+echo "Running disko..."
+# This will format and mount partitions
+disko --mode destroy,format,mount "$FLAKE_DIR/modules/vm-disko-config.nix"
 
 # Ensure EFI partition is mounted correctly
 mkdir -p /mnt/boot/efi
