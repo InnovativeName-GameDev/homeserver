d8c1b595a846649198629083212af161d0a0f499
InnovativeName-GameDev <david.simon11melone@gmail.com>
2026-03-07 10:49:33 +0100

    iso test

--- iso/iso.nix
+++ iso/iso.nix
@@ -16,12 +16,6 @@
 
   # Enable networking
   networking.networkmanager.enable = true;
-  networking.nameservers = [
-    "1.1.1.1"
-    "1.0.0.1"
-    "8.8.8.8"
-    "8.8.4.4"
-  ];
 
   # Define your hostname.
   networking.hostName = "pve-nixos-installer";
