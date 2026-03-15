be6e2d9123a95e04c8c1368fa6d6c59ddf7302e6
InnovativeName-GameDev <david.simon11melone@gmail.com>
2026-03-15 15:17:19 +0100

    Homeserver VM Test

--- hosts/homeserver-alfa-test/configuration.nix
+++ hosts/homeserver-alfa-test/configuration.nix
@@ -437,19 +437,6 @@
     enable = true;
   };
 
-  virtualisation.oci-containers.containers = {
-    #open-webui = {
-    #  image = "ghcr.io/open-webui/open-webui:v0.4.5"; # Pinned to stable version
-    #  ports = [ "3000:8080" ];
-    #  extraOptions = [ "--add-host=host.docker.internal:host-gateway" ];
-    #};
-
-    searxng = {
-      image = "searxng/searxng:latest";
-      ports = [ "8888:8080" ];
-    };
-  };
-
   services.ollama = {
     enable = true;
     openFirewall = false;
